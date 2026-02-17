9887718b49c54ae07f350b8e88d88b8a174ffe83
ealvesjr90 <ealvesjr90@gmail.com>
2026-02-17 13:30:55 -0300

    terceiro oci

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,3 @@
 provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = "~/.oci/oci_api_key.pem"
-  region           = var.region
+  config_file_profile = "DEFAULT"
 }
