e94e8373f7b22a75cde24f662c6c7dbf08cae0bc
ealvesjr90 <ealvesjr90@gmail.com>
2026-02-18 08:43:01 -0300

    segundo teste oci

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,7 @@
 provider "oci" {
-  config_file_profile = "DEFAULT"
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = "~/.oci/oci_api_key.pem"
+  region           = var.region
 }
